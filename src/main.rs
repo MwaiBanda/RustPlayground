437f3900bf63f73cb58ae91f89384e201f3d7e11
Mwai Banda <mwai.banda@cbn.org>
2023-08-26 17:50:52 -0500

    Played with deserializing & serializing between json & structs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Person {
     name: String,
     age: u8,
@@ -10,6 +13,18 @@ fn main(){
         age: 32,
     };
     println!("Hello, {:?}!", person);
+
+    let json = json!([{
+        "name": "Mwai Banda",
+        "age": 22,
+    }]);
+
+    let json_str = serde_json::to_string(&person).unwrap();
+    
+    let p2: Vec<Person> = serde_json::from_value(json).unwrap();
+    
+    println!("{:?}", json_str);
+    println!("{:?}", p2);
     filter_int(|x| x > 5);
 
     (0..10).for_each(|x| print!("{}", x));
